f9be9402d9b852f8d34c2425efb3e5b18bd06915
Ewuradj <comfortearthur69@gmail.com>
2024-04-22 13:39:39 +0000

    add changes to andd wishlist

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 
 import './App.css';
-import HomePage from './pages/home/homePage';
-import InterestCategory from './sections/InterestCategory';
-import OnboardingPage from './sections/Onboarding';
-import SignUpPage from './sections/SignUpPage';
-import SplashScreen from './sections/SplashScreen';
-import AllCategoriesCard from './sections/homesections/AllCategories';
-import BestDealsCard from './sections/homesections/BestDeal';
-import DiscountCard from './sections/homesections/Discount';
+import Wishlist from './pages/home/wishlistPage/wishlistPage';
+import Store from './sections/homesections/store';
 
-import LoginPage from './sections/loginPage';
+// import InterestCategory from './sections/InterestCategory';
+// import OnboardingPage from './sections/Onboarding';
+// import SignUpPage from './sections/SignUpPage';
+// import SplashScreen from './sections/SplashScreen';
+// import AllCategoriesCard from './sections/homesections/AllCategories';
+// import BestDealsCard from './sections/homesections/BestDeal';
+// import DiscountCard from './sections/homesections/Discount';
+
+// import LoginPage from './sections/loginPage';
 // import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import InterestOptionPage from './sections/InterestOptions';
 // import OnboardingPage from './sections/Onboarding';
@@ -30,18 +32,21 @@ import LoginPage from './sections/loginPage';
 function App() {
 
   return (
-   <>
-   <SplashScreen />
-   <OnboardingPage/>
-   <SignUpPage />
-   <LoginPage />
-   <InterestCategory />
-   <AllCategoriesCard />
-   <BestDealsCard />
-   <DiscountCard />
-   <HomePage />
-  
+  <>
+  <Store/>
+  <Wishlist/>
   </>
+  //  <>
+  //  <SplashScreen />
+  //  <OnboardingPage/>
+  //  <SignUpPage />
+  //  <LoginPage />
+  //  <InterestCategory />
+  //  <AllCategoriesCard />
+  //  <BestDealsCard />
+  //  <DiscountCard 
+  
+  
   );
 
 }
